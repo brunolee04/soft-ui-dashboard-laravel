4fa4e411f7103468a5bfa454d5ba11c24e57f0aa
unknown <brunolee04@gmail.com>
2023-01-09 19:03:17 -0300

    09/01/23 19:03

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller{
+
+    public function getMovies() {
+        // logic to get all students goes here
+        return response()->json([
+            "message" => "student record created"
+        ], 201);
+      }
+  
+      public function createStudent(Request $request) {
+        // logic to create a student record goes here
+      }
+  
+      public function getMovie($id) {
+        // logic to get a student record goes here
+      }
+  
+      public function updateStudent(Request $request, $id) {
+        // logic to update a student record goes here
+      }
+  
+      public function deleteStudent ($id) {
+        // logic to delete a student record goes here
+      }
+}
--- resources/views/movie/register.blade.php
+++ resources/views/movie/register.blade.php
@@ -16,74 +16,34 @@
               <div class="table-responsive p-0">
  
 
-              <div class="ct-example" style="position: relative;border: 2px solid #f5f7ff !important;border-bottom: none !important;padding: 1rem 1rem 2rem 1rem;">
+          <div class="ct-example" style="position: relative;border: 2px solid #f5f7ff !important;border-bottom: none !important;padding: 1rem 1rem 2rem 1rem;">
             <form>
+
+              
               <div class="row">
                 <div class="col-md-6">
-                  <div class="form-group">
-                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" onfocus="focused(this)" onfocusout="defocused(this)">
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <input type="text" placeholder="Regular" class="form-control" disabled="" onfocus="focused(this)" onfocusout="defocused(this)">
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" onfocus="focused(this)" onfocusout="defocused(this)">
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <input type="text" placeholder="Regular" class="form-control" disabled="" onfocus="focused(this)" onfocusout="defocused(this)">
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" onfocus="focused(this)" onfocusout="defocused(this)">
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <input type="text" placeholder="Regular" class="form-control" disabled="" onfocus="focused(this)" onfocusout="defocused(this)">
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <div class="input-group mb-4">
-                      <span class="input-group-text"><i class="ni ni-zoom-split-in"></i></span>
-                      <input class="form-control" placeholder="Search" type="text" onfocus="focused(this)" onfocusout="defocused(this)">
+                  <!-- Movie Form -->
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <input type="email" class="form-control"  placeholder="Título" onfocus="focused(this)" onfocusout="defocused(this)">
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <input type="text" placeholder="Regular" class="form-control"  onfocus="focused(this)" onfocusout="defocused(this)">
+                      </div>
                     </div>
                   </div>
+                  <!-- Movie Form -->
                 </div>
                 <div class="col-md-6">
-                  <div class="form-group">
-                    <div class="input-group mb-4">
-                      <input class="form-control" placeholder="Birthday" type="text" onfocus="focused(this)" onfocusout="defocused(this)">
-                      <span class="input-group-text"><i class="ni ni-zoom-split-in"></i></span>
-                    </div>
-                  </div>
+                  <!-- Movie Additional -->
                 </div>
               </div>
-              <div class="row">
-                <div class="col-md-6">
-                  <div class="form-group has-success">
-                    <input type="text" placeholder="Success" class="form-control is-valid" onfocus="focused(this)" onfocusout="defocused(this)">
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="form-group has-danger">
-                    <input type="email" placeholder="Error Input" class="form-control is-invalid" onfocus="focused(this)" onfocusout="defocused(this)">
-                  </div>
-                </div>
-              </div>
+
+            
+
             </form>
           </div>
 

45b6d2996d6aed773266de1fcf7fe29a19e05d07
brunolee04 <brunolee04@gmail.com>
2023-02-07 17:55:31 -0300

    07/02/23 17:55

--- app/Http/Controllers/SystemMovieListController.php
+++ app/Http/Controllers/SystemMovieListController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+use App\Models\SystemMovieList;
+use App\Models\SystemMovieListDescription;
+
 class SystemMovieListController extends Controller{
 
     private $language_id = 0;
@@ -12,8 +15,15 @@ class SystemMovieListController extends Controller{
     private $shows;
     //
 
-    public function listOfSystemLists(){      
-        return view('system_list/list');
+    public function listOfSystemLists(){    
+        $this->language_id = config('app.language_id');
+
+        $systemLists = DB::table('system_list')
+        ->join('system_list_description', 'system_list.system_list_id', '=', 'system_list_description.system_list_id')
+        ->where('system_list_description.language_id', $this->language_id)
+        ->orderBy('system_list_description.system_list_description_name')
+        ->get();
+        return view('system_list/list',['systemLists'=>$systemLists]);
     }
 
 
@@ -34,12 +44,43 @@ class SystemMovieListController extends Controller{
 
 
     public function save(Request $request){
+        
+        $this->language_id = config('app.language_id');
+
         $system_list_description_name = $request->input('system_list_description_name');
+
         $system_list_description = $request->input('system_list_description');
+
         $shows_to_save = [];
+
         $shows = DB::table('movie')->get();
+
         foreach($shows as $show){
             if($request->input('movie_'.$show->movie_id)) $shows_to_save[] = $request->input('movie_'.$show->movie_id);
         }
+
+        $dbSystemList = new SystemMovieList();
+
+        $dbSystemListDescription = new SystemMovieListDescription();
+
+        $dbSystemList->system_list_date_added = date('Y-m-d');
+
+        $dbSystemList->save();
+
+        $systemListId = $dbSystemList->system_list_id;
+
+        $dbSystemListDescription->system_list_description_name = $system_list_description_name;
+
+        $dbSystemListDescription->system_list_description = $system_list_description;
+
+        $dbSystemListDescription->system_list_id = $systemListId;
+
+        $dbSystemListDescription->language_id = $this->language_id;
+
+        $dbSystemListDescription->save();
+
+        return redirect()->action(
+            [SystemMovieListController::class, 'listOfSystemLists']
+        );
     }
 }

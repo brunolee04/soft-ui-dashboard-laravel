8a9bdfe4a5ac4403c2d43b25ed01846e4e2296ff
unknown <brunolee04@gmail.com>
2023-05-01 09:57:48 -0300

    01/05/23

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->get('homemovies',[ApiController::class, 'homemovies']);
+Route::middleware('auth:sanctum')->get('/homemovies',[ApiController::class, 'homemovies']);
 
 
 

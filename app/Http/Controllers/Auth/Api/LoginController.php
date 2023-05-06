2ca859449c9379ea1407b74639857b604c3e3ee4
unknown <brunolee04@gmail.com>
2023-05-06 14:27:43 -0300

    06/05/23

--- app/Http/Controllers/Auth/Api/LoginController.php
+++ app/Http/Controllers/Auth/Api/LoginController.php
@@ -11,7 +11,7 @@ class LoginController extends Controller{
     public function login(Request $request){
         
         //TO-DO: to valid request
-        $credentials  = $request->only('customer_mail','customer_pass');
+        $credentials  = $request->only('email','password');
 
         if(!auth()->attempt($credentials)) abort(401,'Invalid credentials');
 
--- app/Http/Controllers/Auth/Api/RegisterController.php
+++ app/Http/Controllers/Auth/Api/RegisterController.php
@@ -4,29 +4,41 @@ namespace App\Http\Controllers\Auth\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Carbon\Carbon;
 
-
-
-use App\Models\Customer;
+use App\Models\User;
 
 class RegisterController extends Controller{
     
-    
-    public function register(Request $request,Customer $customer){
+    // public function register(Request $request,Customer $customer){
+        
+    //     //TO-DO: to valid request
+    //     $customerData  = $request->only('customer_firstname','customer_lastname','customer_date_birth','customer_mail','customer_pass','customer_status');
+    //     $customerData['customer_date_birth']  = Carbon::createFromFormat('d/m/Y',$customerData['customer_date_birth'])->format('Y-m-d');
+
+    //     $customerData['customer_pass'] = bcrypt($customerData['customer_pass']);
+      
+    //     if(!$customer = $customer->create($customerData))
+    //         abort(500,'Error to create new customer');
+
+    //     return response()->json([
+    //         'data'=>[
+    //             'customer' => $customer
+    //         ]
+    //     ]);
+    // }
+
+    public function register(Request $request,User $user){
         
         //TO-DO: to valid request
-        $customerData  = $request->only('customer_firstname','customer_lastname','customer_date_birth','customer_mail','customer_pass','customer_status');
-        $customerData['customer_date_birth']  = Carbon::createFromFormat('d/m/Y',$customerData['customer_date_birth'])->format('Y-m-d');
-
-        $customerData['customer_pass'] = bcrypt($customerData['customer_pass']);
+        $userData  = $request->only('name','email','password');
+        $userData['password'] = bcrypt($userData['password']);
       
-        if(!$customer = $customer->create($customerData))
-            abort(500,'Error to create new customer');
+        if(!$user = $user->create($userData))
+            abort(500,'Error to create new user');
 
         return response()->json([
             'data'=>[
-                'customer' => $customer
+                'user' => $user
             ]
         ]);
     }

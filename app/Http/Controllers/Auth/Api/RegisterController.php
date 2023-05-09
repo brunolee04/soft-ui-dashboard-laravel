6af271c7d20a4f97b926e5f49d76a67d1e0b00f1
unknown <brunolee04@gmail.com>
2023-05-09 14:35:04 -0300

    09/05/23 14:35

--- app/Http/Controllers/Auth/Api/RegisterController.php
+++ app/Http/Controllers/Auth/Api/RegisterController.php
@@ -9,38 +9,38 @@ use App\Models\User;
 
 class RegisterController extends Controller{
     
-    // public function register(Request $request,Customer $customer){
-        
-    //     //TO-DO: to valid request
-    //     $customerData  = $request->only('customer_firstname','customer_lastname','customer_date_birth','customer_mail','customer_pass','customer_status');
-    //     $customerData['customer_date_birth']  = Carbon::createFromFormat('d/m/Y',$customerData['customer_date_birth'])->format('Y-m-d');
-
-    //     $customerData['customer_pass'] = bcrypt($customerData['customer_pass']);
-      
-    //     if(!$customer = $customer->create($customerData))
-    //         abort(500,'Error to create new customer');
-
-    //     return response()->json([
-    //         'data'=>[
-    //             'customer' => $customer
-    //         ]
-    //     ]);
-    // }
-
-    public function register(Request $request,User $user){
+    public function register(Request $request,Customer $customer){
         
         //TO-DO: to valid request
-        $userData  = $request->only('name','email','password');
-        $userData['password'] = bcrypt($userData['password']);
+        $customerData  = $request->only('customer_firstname','customer_lastname','customer_date_birth','email','password','customer_status');
+        $customerData['customer_date_birth']  = Carbon::createFromFormat('d/m/Y',$customerData['customer_date_birth'])->format('Y-m-d');
+
+        $customerData['customer_pass'] = bcrypt($customerData['customer_pass']);
       
-        if(!$user = $user->create($userData))
-            abort(500,'Error to create new user');
+        if(!$customer = $customer->create($customerData))
+            abort(500,'Error to create new customer');
 
         return response()->json([
             'data'=>[
-                'user' => $user
+                'customer' => $customer
             ]
         ]);
     }
+
+    // public function register(Request $request,User $user){
+        
+    //     //TO-DO: to valid request
+    //     $userData  = $request->only('name','email','password');
+    //     $userData['password'] = bcrypt($userData['password']);
+      
+    //     if(!$user = $user->create($userData))
+    //         abort(500,'Error to create new user');
+
+    //     return response()->json([
+    //         'data'=>[
+    //             'user' => $user
+    //         ]
+    //     ]);
+    // }
 
 }

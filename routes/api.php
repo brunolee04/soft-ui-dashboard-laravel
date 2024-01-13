0def68a97c2c12de16d0ad33a052b0748a1341e8
unknown <brunolee04@gmail.com>
2024-01-13 13:25:09 -0300

    update upi

--- routes/api.php
+++ routes/api.php
@@ -49,7 +49,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
 /**
  * Auth Routes, don´t need to be authenticated to access
  */
-Route::group(['middleware' => 'cors'], function () {
+//Route::group(['middleware' => 'cors'], function () {
+    Route::group(['prefix' => 'auth', 'middleware' => ['cors']], function(){
     Route::post('/login',[\App\Http\Controllers\Auth\Api\LoginController::class,'login']);
 
     Route::post('/logout',[\App\Http\Controllers\Auth\Api\LoginController::class,'logout']);

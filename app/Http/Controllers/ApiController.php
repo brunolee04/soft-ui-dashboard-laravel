937218a2c62e4c0d3cb4c5b5fad5372b8e26deb0
brunolee04 <brunolee04@gmail.com>
2024-02-08 13:42:26 -0300

    08/02

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -358,6 +358,18 @@ class ApiController extends Controller{
         $show_id = $inputs['show_id'];
         $list_id = $inputs['list_id'];
 
+        //1º - Checks if show is already in a List
+
+        $db_my_list_info = DB::table('customer_list')
+        ->join('movie_to_customer_list', 'customer_list.customer_list_id', '=', 'movie_to_customer_list.customer_list_id')
+        ->where('movie_to_customer_list.customer_list_id','=',$list_id)
+        ->where('movie_to_customer_list.movie_id','=',$show_id)
+        ->get();
+
+        if($db_my_list_info){
+          $db_my_list_info->delete();
+        }
+
         $dbShowToCustomerList = new MovieToCustomerList();
 
         $dbShowToCustomerList->customer_list_id = $list_id;

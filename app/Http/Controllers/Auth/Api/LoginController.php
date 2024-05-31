7f5776c74078df75bdb61994db80d2896485d46a
unknown <brunolee04@gmail.com>
2024-05-31 17:45:10 -0300

    31/05

--- app/Http/Controllers/Auth/Api/LoginController.php
+++ app/Http/Controllers/Auth/Api/LoginController.php
@@ -19,6 +19,7 @@ class LoginController extends Controller{
 
         $user_id = auth('customer')->user()->customer_id;
         $user_name = auth('customer')->user()->customer_firstname;
+        $user_lastname = auth('customer')->user()->customer_lastname;
         $user_mail = auth('customer')->user()->email;
         $user_profile_id = auth('customer')->user()->customer_user_id;
         $user_avatar = auth('customer')->user()->customer_image_url;
@@ -33,6 +34,7 @@ class LoginController extends Controller{
                 'user' => [
                     'id' => $user_id,
                     'name' => $user_name,
+                    'lastname' => $user_lastname,
                     'mail' => $user_mail,
                     'profile_id' => $user_profile_id,
                     'avatar' => $user_avatar,

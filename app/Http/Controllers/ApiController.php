8fcf47eedd793da0f66a76ba3b48a21bb0e17082
brunolee04 <brunolee04@gmail.com>
2024-06-03 17:05:25 -0300

    03/06

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -62,9 +62,9 @@ class ApiController extends Controller{
         $image = $image[0];
         //php artisan storage:link
       
-        $this->validate($image, [
-          'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
-        ]);
+        // $this->validate($image, [
+        //   'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+        // ]);
   
        // $image_url = url(Storage::url($image['image']->store('image', 'public')));
 

f6b0234b5d80458b26d750de8452cc640e5bfeef
brunolee04 <brunolee04@gmail.com>
2024-06-03 16:56:25 -0300

    03/06

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -61,11 +61,11 @@ class ApiController extends Controller{
         $image = $inputs["formData"];
         $image = $image[0];
         //php artisan storage:link
-     /*   
+      
         $this->validate($image, [
           'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
         ]);
- 
+  /* 
         $image_url = url(Storage::url($request->file('image')->store('image', 'public')));
 */
         //customer_image_url //atualizar imagem na tabela do cliente

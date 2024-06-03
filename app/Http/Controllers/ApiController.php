fca145983c1d4a5b079205a5fe9cd9d6d6c9335d
brunolee04 <brunolee04@gmail.com>
2024-06-03 17:23:50 -0300

    03/06

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -73,12 +73,12 @@ class ApiController extends Controller{
         //   'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
         // ]);
   
-       // $image_url = url(Storage::url($image['image']->store('image', 'public')));
+        $image_url = url(Storage::url($image['uri']->store('image', 'public')));
 
         //customer_image_url //atualizar imagem na tabela do cliente
         return response()->json([
           "status"  => true,
-          "data"    => $image
+          "data"    => $image_url
         ], 201);
     }
 

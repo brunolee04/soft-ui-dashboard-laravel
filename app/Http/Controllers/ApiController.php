d128d85e3ef456a7c3a9de14b0c5675e3c1c1d51
unknown <brunolee04@gmail.com>
2024-06-03 21:33:24 -0300

    02/06

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -57,21 +57,18 @@ class ApiController extends Controller{
 
       public function sendImage(Request $request){
         //https://laracasts.com/discuss/channels/laravel/how-to-save-image-as-blob
-        $inputs = $request->all();
-        // $inputs = $inputs['formData'];
-        // $inputs = $inputs['_parts'];
-        // $inputs = $inputs[0];
-        //php artisan storage:link
-        // $this->validate($inputs['image'], [
-        //   'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
-        // ]);
 
-      //  $image_url = url(Storage::url($inputs[1]->store('image', 'public')));
+        //php artisan storage:link
+        $this->validate($request, [
+          'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+        ]);
+
+        $image_url = url(Storage::url($request->file('image')->store('image', 'public')));
 
         //customer_image_url //atualizar imagem na tabela do cliente
         return response()->json([
           "status"  => true,
-          "data"    => $inputs
+          "data"    => $image_url
         ], 201);
     }
 

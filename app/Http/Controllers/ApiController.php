f372fab3aa200e219b5df9a6e72a7e2358751b1d
unknown <brunolee04@gmail.com>
2024-06-26 18:36:36 -0300

    26/06

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -485,7 +485,7 @@ class ApiController extends Controller{
               ->join('movie_to_customer_list', 'movie.movie_id', '=', 'movie_to_customer_list.movie_id');
               
                 if(is_array($genderFilterValues) && count($genderFilterValues) > 0){
-                  return $db_show_data->join('movie_to_movie_gender', 'movie.movie_id', '=', 'movie_to_movie_gender.movie_id');
+                 // return $db_show_data->join('movie_to_movie_gender', 'movie.movie_id', '=', 'movie_to_movie_gender.movie_id');
                 }
                
               // ->join('movie_to_movie_gender',  function($query) use ($genderFilterValues){

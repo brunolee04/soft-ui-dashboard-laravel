c5e6075d3fa2a8e93b0fa2aa519227f212648337
brunolee04 <brunolee04@gmail.com>
2024-07-01 11:45:56 -0300

    01/07

--- routes/api.php
+++ routes/api.php
@@ -60,6 +60,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     //List apis
     Route::get('mylists',[ApiController::class,'getMyLists']);
     Route::get('/myList/getMyListById/{listId}',[ApiController::class,'getMyListById']);
+    Route::post('/myList/delMyList',[ApiController::class,'delMyList']);
     Route::get('getMyListsWithShows/{customer_id}',[ApiController::class,'getMyListsWithShows']);
     Route::post('/myList/addShow',[ApiController::class, 'setShowToMyList']);
     Route::post('/myList/saveNewList',[ApiController::class, 'saveNewList']);
